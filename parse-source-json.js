df0dbe489ea2445d13d712498bb22ec4f8e91ce5
Adam Williams <1995adamwilliams@gmail.com>
2020-03-29 23:39:07 +0100

    - Replace '\r\n' in note content with HTML linebreak - Fix date formatting by using moment package - escape XML in note content

--- parse-source-json.js
+++ parse-source-json.js
@@ -1,34 +1,62 @@
 const fs = require('fs');
+const moment = require('moment');
 
-const filePath = process.argv.slice(2)[0];
-const source = fs.readFileSync(filePath);
+const exportFilePath = process.argv.slice(2)[0];
+const simplenoteExport = fs.readFileSync(exportFilePath);
 
-const notes = JSON.parse(source);
-const testNote = notes.activeNotes[1];
-const trashedNotes = notes.trashedNotes;
+const notes = JSON.parse(simplenoteExport);
+const {activeNotes, trashedNotes} = notes;
 
 const finalFile = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE en-export SYSTEM \"http://xml.evernote.com/pub/evernote-export3.dtd\">
 <en-export export-date="20171228T194211Z" application="Evernote" version="Evernote Mac 6.13.3 (455969)">`;
 
-// maybe i don't need to replace
-// function replaceNewlineCharacters(str) {
-//     text.replace(' \r\n', 'ferret')
-// }
+async function replaceNewlineChars(str) {
+    return str.replace(/\r\n/g, '<br/>');
+}
 
-function isoToEnDate(isoDatestring) {
+function isoToEvernoteDate(isoDatestring) {
     var d = new Date(isoDatestring);
-    return `${d.getFullYear()}${d.getMonth()}${d.getDay()}T${d.getHours()}${d.getMinutes()}${d.getSeconds()}Z`
+    var d = moment(d).format('YYYYMMDDTHHmmss[Z]');
+    return d;
 }
 
-function simplenoteToEnexFormat(noteJson) {
+async function escapeXml(unsafe) {
+    return unsafe.replace(/[<>&'"]/g, function (c) {
+        switch (c) {
+            case '<': return '&lt;';
+            case '>': return '&gt;';
+            case '&': return '&amp;';
+            case '\'': return '&apos;';
+            case '"': return '&quot;';
+        }
+    });
+}
+
+async function simplenoteToEnexFormat(noteJson) {
     var {content, creationDate, lastModified} = noteJson;
-    var creationDate = isoToEnDate(creationDate);
-    var lastModified = isoToEnDate(lastModified);
-    const title = content.slice(0, 6);
-    return `<note><title>${title}</title><content><![CDATA[<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note>${content}</en-note>]]></content><created>${creationDate}</created><updated>${lastModified}</updated><note-attributes><author></author><source></source><reminder-order>0</reminder-order></note-attributes></note></en-export>`
+    var creationDate = isoToEvernoteDate(creationDate);
+    var lastModified = isoToEvernoteDate(lastModified);
+    var content = await escapeXml(content);
+    var content = await replaceNewlineChars(content);
+    const title = content.split('<br/>')[0];
+    return `<note><title>${title}</title><content><![CDATA[<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note>${content}</en-note>]]></content><created>${creationDate}</created><updated>${lastModified}</updated><note-attributes><author></author><source></source><reminder-order>0</reminder-order></note-attributes></note>`
 }
 
-const enex = simplenoteToEnexFormat(testNote);
+async function createNotebookFileContent(testNotes) {
+    var result = await Promise.all(testNotes.map(async function (x) {
+        return await simplenoteToEnexFormat(x);
+    }));
+    var result =  result.join('');
+    return finalFile + result + '</en-export>';
+}
 
-fs.writeFileSync('enex.enex', finalFile + enex);
+async function run() {
+    var activeNotesResult = await createNotebookFileContent(activeNotes);
+    var trashedNotesResult = await createNotebookFileContent(trashedNotes);
+
+    fs.writeFileSync('activeNotes.enex', activeNotesResult);
+    fs.writeFileSync('trashedNotes.enex', trashedNotesResult);
+}
+
+run();
